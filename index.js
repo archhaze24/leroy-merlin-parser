0a62249f7a398a3c79524405cebb92c85ceff83f
Kirill Belolipetsky <archhaze24@gmail.com>
2024-07-30 16:50:43 +0300

    change timeout to 5 seconds

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ async function parseCategoriesAndSaveInDb(page, db, state) {
     for (const secondLevelCategory of secondLevelCategories) {
         while (true) {
             console.log(`getting categories from route ${secondLevelCategory}`);
-            await new Promise((resolve) => setTimeout(resolve, 2000));
+            await new Promise((resolve) => setTimeout(resolve, 5000));
             try {
                 const state = await getState(
                     page,
@@ -167,7 +167,7 @@ async function parseProductsAndSaveInDb(page, db) {
                 for (let pageNumber = 1; pageNumber <= pages; pageNumber++) {
                     while (true) {
                         try {
-                            await new Promise((resolve) => setTimeout(resolve, 2000));
+                            await new Promise((resolve) => setTimeout(resolve, 5000));
                             console.log(`getting products from category ${category.url}, page ${pageNumber}`)
                             const state = await getState(page, `https://lemanapro.ru${category.url}/?page=${pageNumber}`);
                             const products = state.plp.plp.plp.products.productsList;
